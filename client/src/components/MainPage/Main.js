309c657f75e8f020327b4337736047fd4478541a
김재현 <gimjaehyeon@gimjaehyeon-ui-MacBookPro.local>
2023-02-21 10:33:39 +0900

    prc

---
+++ client/src/components/MainPage/Main.js
@@ -0,0 +1,80 @@
+import {Content,Container, Mainbar,Main1div,Main1h1,Main1div1,Main1div1a, Main2div,Main2div1,Main2div2,Main2div21,Main2div21a, Main2div21a1,Main2div21a1span,
+        Main2div21a2,Main2div21a2L,Mainbarbr,Mainbarh2,Mianbarh2a,Mainpn,Mainpndiv,Mainpndiva,Mainpndiva1,Mainpn2,Mainpn2a ,Mainpn2a1,Mainpnspan} from './Style/Style.js'
+import {MainSidebar} from './component/Sidebar.js'
+import Mainscript from "./component/Mainpage.js"
+import { useEffect, useState } from 'react';
+
+
+function Main(){
+
+    return (
+        <Container>
+        <Content>
+            <Mainbar>
+                <Main1div>
+                    <Main1h1>Top Question</Main1h1>
+                    <Main1div1>
+                        <Main1div1a href="/questions/ask">Ask Question</Main1div1a>
+                    </Main1div1>
+                </Main1div>
+                <Main2div>
+                    <Main2div1></Main2div1>
+                    <Main2div2>
+                        <Main2div21>
+                            <Main2div21a>Interesting</Main2div21a>
+                            <Main2div21a1><Main2div21a1span>281</Main2div21a1span>"Bountied"</Main2div21a1>
+                            <Main2div21a2>Hot</Main2div21a2>
+                            <Main2div21a2>Week</Main2div21a2>
+                            <Main2div21a2L>Month</Main2div21a2L>
+                        </Main2div21>
+                    </Main2div2>
+                </Main2div> 
+
+
+                    {/* 내용 map으로 뿌리기 전  */}
+                         <Mainscript/>
+                         <Mainscript/>
+                         <Mainscript/>
+                         <Mainscript/>
+                         <Mainscript/>
+        
+                <Mainbarbr></Mainbarbr>
+                <Mainbarh2>"Looking for more? Browse the "
+                    <Mianbarh2a>
+                    complete list of questions
+                    </Mianbarh2a>
+                    "or"
+                    <Mianbarh2a>popular tags</Mianbarh2a>
+                    ". Help us answer"
+                    <Mianbarh2a>unanswered questions</Mianbarh2a>
+                </Mainbarh2>
+                <script></script>
+            </Mainbar>
+
+            <Mainpn>
+                <Mainpndiva>15</Mainpndiva>
+                <Mainpndiva1>30</Mainpndiva1>
+                <Mainpndiva1>50</Mainpndiva1>
+                <Mainpnspan>per page</Mainpnspan>
+            </Mainpn>
+             <Mainpn2>
+                <Mainpn2a>1</Mainpn2a>
+                <Mainpn2a1>2</Mainpn2a1>
+                <Mainpn2a1>3</Mainpn2a1>
+                <Mainpn2a1>4</Mainpn2a1>
+                <Mainpn2a1>5</Mainpn2a1>
+                <a>...</a>
+                <Mainpn2a1>1567342</Mainpn2a1>
+                <Mainpn2a1> Next</Mainpn2a1>
+            </Mainpn2>
+            
+
+           
+            
+        </Content>
+        <MainSidebar/>
+        </Container>
+    )
+}
+
+export {Main}
