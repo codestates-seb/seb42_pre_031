801b9fc80c2090096c20e63cddbcb9b010690ac1
ByeongminBRO <abm40172138@gmail.com>
2023-02-21 11:00:09 +0900

    가입, 로그인, 로그아웃 기능X

---
+++ client/src/Login.js
@@ -0,0 +1,178 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+
+const Container = styled.div`
+  background-color: #f1f2f3;
+  width: 100%;
+  min-height: 800px;
+  box-sizing: border-box;
+`;
+
+
+
+const Content = styled.div`
+  min-width: 300px;
+  width: 15%;
+  margin: 0 auto;
+  padding-top: 5%;
+`;
+const SocialSignupContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  width: 350px;
+  height: 20%;
+  margin-top: 100px;
+  font-size: 0.7rem;
+  `;
+const SocialButton = styled.button`
+  width: 100%;
+  margin: 0.5em;
+  height: 40px;
+  border-radius: 7px;
+  background-color: ${(props) => props.color};
+  color: ${(props) => props.fontcolor};
+  font-size: 1em;
+  border-color: #dcdcdc;
+`;
+
+
+const InputForm = styled.div`
+  background-color: white;
+  border: 1px solid #d6d6d6;
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.3);
+  border-radius: 5px;
+  margin: 5px;
+  width: 100%;
+  padding: 20px;
+`;
+
+const InputLable = styled.div`
+  padding: 5px;
+  p {
+    font-weight: bold;
+    font-size: 1rem;
+    padding: 10px 0;
+  }
+  input {
+    width: 100%;
+    padding: 10px 0;
+    &:focus {
+      box-shadow: 0 0 5px 5px rgba(28, 107, 138, 0.3);
+    }
+  }
+  span {
+    color: #999;
+    font-size: 12px;
+    margin-top: 10px;
+  }
+  > div {
+    display: flex;
+    justify-content: space-between;
+    a {
+      margin-top: 10px;
+      color: #0074cc;
+      text-decoration: none;
+      font-size: 12px;
+    }
+  }
+`;
+
+const LoginButton = styled.button`
+  background-color: #0a95ff;
+  border: none;
+  width: 100%;
+  border-radius: 3px;
+  padding: 10px 20px;
+  margin: 10px 0;
+  color: white;
+  cursor: pointer;
+`;
+
+const SignupGuide = styled.div`
+  text-align: center;
+  font-size: 12px;
+  margin: 40px 0;
+  p {
+    margin: 20px 0;
+  }
+  a {
+    color: #0074cc;
+    text-decoration: none;
+  }
+`;
+
+function Login() {
+
+
+
+  
+
+  // const emailRegex =
+  //   /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+  // async function getLogin() {
+  //   if (!emailRegex.test(account.email)) {
+  //     Alert('error', 'The email is not a valid email address.');
+  //   } else if (account.password.length < 4) {
+  //     Alert('error', 'Please enter at least 4 characters for the password.');
+  //   } else {
+
+  //       
+  //        
+  //     }
+  //   }
+  // }
+
+  return (
+    <Container>
+
+      <Content>
+          <SocialSignupContainer>
+          <SocialButton color="white">Log in with Google</SocialButton>
+          <SocialButton color="#23262A" fontcolor="white">
+            Log in with GitHub
+          </SocialButton>
+          <SocialButton color="#293D83" fontcolor="white">
+            Log in with Facebook
+          </SocialButton>
+        </SocialSignupContainer>
+        <InputForm>
+          <InputLable>
+            <p>Email</p>
+            <input
+              name="password"
+              type="text"
+            />
+          </InputLable>
+          <InputLable>
+            <div>
+              <p>Password</p>
+              <a href="/signup">Forgot password?</a>
+            </div>
+            <input
+              name="password"
+              type="password"
+            />
+          </InputLable>
+          <LoginButton >
+            Log in
+          </LoginButton>
+        </InputForm>
+        <SignupGuide>
+          <p>
+            Don’t have an account?<a href="/"> Sign up</a>
+          </p>
+          <p>
+            Are you an employer?<a href="/"> Sign up on Talen</a>
+          </p>
+        </SignupGuide>
+      </Content>
+    </Container>
+  );
+}
+
+export default Login;
+
---
+++ client/src/SignUp.js
@@ -0,0 +1,147 @@
+import styled from "styled-components";
+import React from "react";
+
+
+const Main = styled.div`
+  display: flex;
+  background-color: #eff0f1;
+  height: 900px;
+  justify-content: center;
+  flex-direction: row;
+`;
+const Explanation = styled.div`
+  width: 400px;
+`;
+
+const Block = styled.div`
+  display: flex;
+  margin-top: 300px;
+  flex-direction: column;
+`;
+const ExTitle = styled.span`
+  font-weight: 600;
+  font-size: 1.4rem;
+`;
+const ExDetail = styled.span`
+  font-size: 0.8rem;
+  margin-top: 20px;
+`;
+
+const SocialSignupContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0;
+  width: 350px;
+  height: 20%;
+  margin-top: 100px;
+  font-size: 0.7rem;
+`;
+
+const SignupContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  height: 450px;
+  background-color: white;
+  border-radius: 5%;
+  box-shadow: -1px 1px 10px 2px #dcdcdc;
+  font-size: 0.7rem;
+  font-weight: 300;
+`;
+
+const SocialButton = styled.button`
+  width: 100%;
+  margin: 0.5em;
+  height: 40px;
+  border-radius: 7px;
+  background-color: ${(props) => props.color};
+  color: ${(props) => props.fontcolor};
+  font-size: 1em;
+  border-color: #dcdcdc;
+`;
+const Button = styled.button`
+  background-color: #2d7fff;
+  border: none;
+  width: 80%;
+  height: 40px;
+  margin-left: 35px;
+  border-radius: 4px;
+  text-align: center;
+  color: white;
+  font-size: 1.2em;
+  font-weight: 300;
+`;
+const Input = styled.input`
+  margin-left: 35px;
+  width: 80%;
+  height: 10%;
+  padding: 8px 32px 8px 9px;
+  border: 1px solid #babfc4;
+  font-size: 16px;
+  line-height: 300;
+  border-radius: 4px;
+`;
+
+const Div = styled.div`
+  font-weight: 500;
+  margin-top: 30px;
+  margin-bottom: 5px;
+  margin-left: 35px;
+`;
+const Span = styled.div`
+  margin: 10px 0 10px 35px;
+  width: 80%;
+  font-size: 14px;
+  color: #6a737c;
+`;
+function SignUp() {
+
+  return (
+    <Main>
+
+      <Explanation>
+        <Block>
+          <ExTitle>Join the Stack Overflow community</ExTitle>
+          <ExDetail>Get unstuck - ask a question</ExDetail>
+          <ExDetail>Unlock new privileges like voting and commenting</ExDetail>
+          <ExDetail>Save your favorite tags, filters, and jobs</ExDetail>
+          <ExDetail>Earn reputation and badges</ExDetail>
+        </Block>
+      </Explanation>
+   
+      <Explanation>
+        <SocialSignupContainer>
+          <SocialButton color="white">Sign up with Google</SocialButton>
+          <SocialButton color="#23262A" fontcolor="white">
+            Sign up with GitHub
+          </SocialButton>
+          <SocialButton color="#293D83" fontcolor="white">
+            Sign up with Facebook
+          </SocialButton>
+        </SocialSignupContainer>
+        <SignupContainer>
+          <form>
+            <Div>Display name</Div>
+            <Input></Input>
+            <Div>Email</Div>
+            <Input
+              type="text"
+            ></Input>
+            <Div>Password</Div>
+            <Input
+              type="password"
+            ></Input>
+            <Span>
+              Passwords must contain at least eight characters, including at
+              least 1 letter and 1 number.
+            </Span>
+            <Button type="submit">Sign up</Button>
+          </form>
+        </SignupContainer>
+      </Explanation>
+    </Main>
+  );
+}
+
+export default SignUp;
